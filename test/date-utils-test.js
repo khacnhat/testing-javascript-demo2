3e84fcf928398c9933075d7552041c72718c9646
Nhat Nguyen Khac <nhat@Nhats-MBP.localdomain>
2019-02-20 14:07:18 +0700

    Add test coverage

--- test/date-utils-test.js
+++ test/date-utils-test.js
@@ -2,8 +2,14 @@ let DateUtils = require('../src/date-utils');
 let assert = require('assert');
 let sinon = require('sinon');
 
-it('2/2019 should return 28 days', function () {
+it('2/2020 should return 28 days', function () {
     let du = new DateUtils();
     sinon.stub(du, 'isLeapYear').returns(true);
     assert.equal(du.getDaysOfMonth(2, 2020), 29);
+});
+
+it('2/2019 should return 28 days', function () {
+    let du = new DateUtils();
+    sinon.stub(du, 'isLeapYear').returns(false);
+    assert.equal(du.getDaysOfMonth(2, 2020), 28);
 });
